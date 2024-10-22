ddf0803de97822b15eb5aa2621ba8ae6f5305ac1
CrayzeH <vvsdfvvs@gmail.com>
2024-10-22 13:24:38 +0500

    Первый апдейт

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-print('hello world')
+app = Flask(__name__)
+app.secret_key = 'insanity'
+
+@app.route('/')
+def hello_world():
+  return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run()
