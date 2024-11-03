459ba95ae6ccb2ec8ce252f01bf7ef840fe8e582
CrayzeH <vvsdfvvs@gmail.com>
2024-11-03 18:00:42 +0500

    обновка

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ def add_user(name, email, password, phone):
 def check_user_by_email(email, password):
     conn = sqlite3.connect('everlessdata')
     c = conn.cursor()
-    c.execute(f"SELECT email, password FROM users WHERE  email = '{email}' AND password = '{password}'")
+    c.execute(f"SELECT id, email, password FROM users WHERE  email = '{email}' AND password = '{password}'")
     is_user = c.fetchall()
     conn.close()
     return is_user
@@ -18,7 +18,15 @@ def check_user_by_email(email, password):
 def check_user_by_phone(phone, password):
     conn = sqlite3.connect('everlessdata')
     c = conn.cursor()
-    c.execute(f"SELECT phone, password FROM users WHERE  phone = '{phone}' AND password = '{password}'")
+    c.execute(f"SELECT id, phone, password FROM users WHERE  phone = '{phone}' AND password = '{password}'")
     is_user = c.fetchall()
     conn.close()
     return is_user
+
+def get_user_data(id):
+    conn = sqlite3.connect('everlessdata')
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM users WHERE id = {id}")
+    user_data = c.fetchall()
+    conn.close()
+    return user_data
