a4b8ac9ee5c3c2e831b5ccca1accecb619e5f3bc
CrayzeH <vvsdfvvs@gmail.com>
2024-11-12 23:22:48 +0500

    Почти готов адаптив. Ещё 2 страницы. Уже почти всё :PPP

---
+++ static/slider-two.js
@@ -0,0 +1,31 @@
+const slider = document.querySelector('.slider-images');
+    const prevButton = document.querySelector('.first-button');
+    const nextButton = document.querySelector('.second-button');
+    const slides = Array.from(slider.querySelectorAll('img'));
+    const slideCount = slides.length;
+    let slideIndex = 0;
+
+    prevButton.addEventListener('click', showPreviousSlide);
+    nextButton.addEventListener('click', showNextSlide);
+
+    function showPreviousSlide() {
+        slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+        updateSlider();
+    }
+
+    function showNextSlide() {
+        slideIndex = (slideIndex + 1) % slideCount;
+        updateSlider();
+    }
+
+    function updateSlider() {
+        slides.forEach((slide, index) => {
+            if (index === slideIndex) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+
+    updateSlider();
